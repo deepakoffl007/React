54f311ea5366d7b6251f705051e5baace32758f4
deepakoffl007 <deepakelumalai.e@gmail.com>
2026-03-16 10:17:09 +0530

    Delete all

--- login-ui/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { LoginPage } from "@/pages";
-
-const App = () => {
-  return <LoginPage />;
-};
-
-export default App;
--- login-ui/src/components/login/index.js
+++
@@ -1,3 +0,0 @@
-export { default as BackgroundEffects } from "./BackgroundEffects";
-export { default as BrandPanel } from "./BrandPanel";
-export { default as LoginForm } from "./LoginForm";

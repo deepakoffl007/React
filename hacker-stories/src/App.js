e136f44d34059494fb7797a5d7e0feb03a30dc42
deepakoffl007 <deepakelumalai.e@gmail.com>
2026-03-18 10:49:23 +0530

    Learning React : Hello World

---
+++ hacker-stories/src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+function App() {
+  const title = 'React'
+  return (
+    <div>
+      <h1>Hello World {title}</h1>
+      <label html='Forsearch'>Search :</label>
+      <input id='search' type='text'/>
+    </div>
+  );
+
+}
+export default App;
